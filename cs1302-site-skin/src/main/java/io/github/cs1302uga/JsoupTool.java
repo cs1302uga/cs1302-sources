1ab2585c665aa60b40130799ee6d7e600064193d
Michael Cotterell <mepcotterell@gmail.com>
2022-05-08 15:57:15 -0400

    added site skin

---
+++ cs1302-site-skin/src/main/java/io/github/cs1302uga/JsoupTool.java
@@ -0,0 +1,38 @@
+package io.github.cs1302uga;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import org.apache.velocity.tools.config.DefaultKey;
+import org.apache.velocity.tools.generic.SafeConfig;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+/**
+ * An Apache Velocity tool that provides utility methods to manipulate HTML code using
+ * <a href="http://jsoup.org/">jsoup</a> HTML5 parser.
+ *
+ * @author Michael E. Cotterell
+ */
+@DefaultKey("jsoupTool")
+public class JsoupTool extends SafeConfig {
+
+    /**
+     * Construct a {@code JsoupTool} object.
+     */
+    public JsoupTool() {}
+
+    /**
+     * Parse the supplied HTML {@code content} string using jsoup.
+     *
+     * @param content HTML content string
+     * @return An {@link org.jsoup.nodes.Element} that wraps the supplied {@code content}.
+     *
+     */
+    public Element parse(String content) {
+        Document doc = Jsoup.parseBodyFragment(content);
+        return doc.body();
+    } // parse
+
+} // JsoupTool
