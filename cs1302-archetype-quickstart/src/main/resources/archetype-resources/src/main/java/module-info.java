ba8cdf0492c826c544999919a8edebe21eb851a6
Michael Cotterell <mepcotterell@gmail.com>
2024-04-09 17:03:03 -0400

    updated archetype dependencies

--- cs1302-archetype-quickstart/src/main/resources/archetype-resources/src/main/java/module-info.java
+++ cs1302-archetype-quickstart/src/main/resources/archetype-resources/src/main/java/module-info.java
@@ -3,7 +3,9 @@
  * Defines the {@code $groupId:$artifactId} API.
  */
 module $moduleId {
+    requires transitive com.google.gson;
     requires transitive java.logging;
     requires transitive java.net.http;
+    requires transitive javafx.controls;
     exports $package;
 } // $moduleId
